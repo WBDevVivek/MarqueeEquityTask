a745d845dfad6e62b94169e3a01f578cd114023c
WBDev_Vivek <gawalevivek@gmail.com>
2024-06-10 12:20:11 +0530

    routesupdate

--- src/route/routes.jsx
+++ src/route/routes.jsx
@@ -10,7 +10,7 @@ export const router = createBrowserRouter([
         element: <App />
     },
     {
-        path: "bookshelf",
+        path: "/bookshelf",
         element: <BookShelfPage />,
     },
 ]);
